e81a8ba8966622a8d3dffb24e16931b9c3c3941b
Dario Caric <dcaric@me.com>
2024-05-30 16:21:28 +0200

    Initial Commit

---
+++ TestKlasaVektor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestKlasaVektor
+//
+//  Created by Dario Caric on 30.05.2024..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
